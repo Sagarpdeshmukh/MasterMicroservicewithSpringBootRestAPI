e1b093861b12c9027a5284f934c5c93d62c4d840
Sagar <sagarpdeshmukh160@gmail.com>
2024-06-23 01:30:36 +0530

    Added First exception and improvment for Readable message for 404

--- src/main/java/com/example/group/demoArtifact/controller/UserController.java
+++ src/main/java/com/example/group/demoArtifact/controller/UserController.java
@@ -1,37 +1,47 @@
 package com.example.group.demoArtifact.controller;
 
+import com.example.group.demoArtifact.exception.UserNotFoundException;
 import com.example.group.demoArtifact.userDao.User;
 import com.example.group.demoArtifact.userDao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserController {
-private UserDaoService userDao;
-public  UserController(UserDaoService user){
-    this.userDao = user;
+    private UserDaoService userDao;
 
-}
+    public UserController(UserDaoService user) {
+        this.userDao = user;
+
+    }
+
     @GetMapping("/myname")
     public String myName() {
         return "Hi SAGAR PRAKASH DESHMUKH ,,,";
     }
 
     @GetMapping("/all")
-    public List<User> returnAll(){
-        return userDao.userAll();
+    public List<User> returnAll() {
+      return userDao.userAll();
     }
 
     @GetMapping("/findone/{id}")
-    public User getOne(@PathVariable int id){
-    return userDao.findOne(id);
+    public User getOne(@PathVariable int id) {
+      User  od =   userDao.findOne(id);
+        if(od == null) {
+            throw new UserNotFoundException("id "+id);
+        }
+        return userDao.findOne(id);
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody  User user){
-    userDao.save(user);
-
-        return null;
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+      User us =   userDao.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{ids}").buildAndExpand(us.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
