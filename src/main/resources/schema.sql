435ffe509930c481bec34afb52549e2ff0f70090
Sagar <sagarpdeshmukh160@gmail.com>
2024-07-07 22:42:41 +0530

    H2 Database connectivity and video num 295

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table course(id bigint not null,
+                  name varchar (255) not null,
+                  author varchar (255) not null,
+                  primary key (id)
+               );
