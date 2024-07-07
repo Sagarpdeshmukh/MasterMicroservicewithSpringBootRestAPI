dbf81e83046fed821350f5e6368db7a6dac78ae0
Sagar <sagarpdeshmukh160@gmail.com>
2024-07-08 00:07:33 +0530

    Fix issue in JDBC

--- src/main/java/com/example/group/demoArtifact/jdbc/CourseJDBCREpository.java
+++ src/main/java/com/example/group/demoArtifact/jdbc/CourseJDBCREpository.java
@@ -4,34 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @Repository
 public class CourseJDBCREpository {
-@Autowired
-   private JdbcTemplate springJdbcTemplate ;
- private  static String INSERT_QUERY  = """
-               insert into course (id,name,author)
-               values(?,?,?);
-               """;
-   public void  insert(COurse course){
 
-       springJdbcTemplate.update(INSERT_QUERY,course.getId(),course.getName(),course.getAuthor());
-   }
-    private  static String DELETE_QUERY  = """
-               delete from  course where id = ? 
-               """;
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
 
-    public void  delete(long id){
+    private static final String INSERT_QUERY = """
+            insert into course (id, name, author)
+            values(?,?,?);
+            """;
 
-        springJdbcTemplate.update(DELETE_QUERY,id);
+    public void insert(COurse course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
     }
 
-    private  static String SELECT_QUERY  = """
-               select *  from   where  id = ? 
-               """;
+    private static final String DELETE_QUERY = """
+            delete from course where id = ?
+            """;
 
-    public COurse findbyId(long id){
-       return  springJdbcTemplate.queryForObject(SELECT_QUERY,new BeanPropertyRowMapper<>(COurse.class),id);
+    public void delete(long id) {
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
 
+    private static final String SELECT_QUERY = """
+            select * from course where id = ?
+            """;
+
+    public COurse findbyId(long id) {
+        try {
+            return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(COurse.class), id);
+        } catch (EmptyResultDataAccessException e) {
+            return null; // Or throw a custom exception if preferred
+        }
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,5 @@
-create table course(id bigint not null,
-                  name varchar (255) not null,
-                  author varchar (255) not null,
-
-               );
-insert into course (id,name,author)
-values(1,'learn aws','in 28min');
-
-
+CREATE TABLE COurse (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
